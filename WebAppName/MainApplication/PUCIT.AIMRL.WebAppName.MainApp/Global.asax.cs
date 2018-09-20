94a51804fc37dcbf43c60c522f85c462a702a395
bilalshahzad139 <bilal.shahzad@yahoo.com>
2018-09-20 20:06:56 +0500

    MVC version is upgraded from 4 to 5

--- WebAppName/MainApplication/PUCIT.AIMRL.WebAppName.MainApp/Global.asax.cs
+++ WebAppName/MainApplication/PUCIT.AIMRL.WebAppName.MainApp/Global.asax.cs
@@ -27,7 +27,10 @@ namespace PUCIT.AIMRL.WebAppName.MainApp
 
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
